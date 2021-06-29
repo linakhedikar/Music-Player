28748ef210cb38692af7f18e65d5ac92300bf117
LINA KHEDIKAR <linakhedikar151@gmail.com>
2021-06-29 14:48:03 +0530

    Create Music-Player

---
+++ Music-list.js
@@ -0,0 +1,62 @@
+let allMusic = [
+  {
+    name: "Love You Zindagi",
+    artist: "Amit Trivedi,Aaliya Bhatt",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Makhna",
+    artist: "Tanishk Bagchi,Asees Kaur",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Girls Like You",
+    artist: "Maroon 5",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Tere Jaisa Yaar Kahan",
+    artist: "Kishore Kumar",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Dil Ka Koi Tukda",
+    artist: "Hariharan,Kavita Krishnamurthy",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "Tujhse Naraz Nahin Zindagi",
+    artist: "Lata Mangeshkar",
+    img: "music-6",
+    src: "music-6"
+  },
+  {
+    name: "Zindagi Ki Yahi Reet Hai",
+    artist: "Kishore Kumar",
+    img: "music-7",
+    src: "music-7"
+  },
+  {
+    name: "Apna Har Din",
+    artist: "Shaan,Anushka Manchanda",
+    img: "music-8",
+    src: "music-8"
+  },
+  {
+    name: "Sauda Hai Dil Ka Ye",
+    artist: "Anupam Amod, Javed Ali",
+    img: "music-9",
+    src: "music-9"
+  },
+  {
+    name: "Hirwa Nisarga",
+    artist: "Sonu Nigam",
+    img: "music-10",
+    src: "music-10"
+  },
+];
